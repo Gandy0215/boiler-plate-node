b36e31dd09b5c0aaca13321f6505bbdf1da0d458
dongwoo <dongwoo@scgs.co.kr>
2024-02-16 14:36:31 +0900

    feat 회원가입 API 추가
    1. body-parser 추가 설치
        npm install body-parser --save
    2.  /register (POST) API 추가

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const bodyParser = require('body-Parser')
+const { User } = require('./models/User')
+
+//application/x-www-form-urlencoded 파싱을 위해 사용
+app.use(bodyParser.urlencoded({extended: true}))
+
+// application/json 파싱을 위해 사용
+app.use(bodyParser.json())
 
 const mongoose = require('mongoose')
 var db_config = require('./conf/mongo-config.json')
@@ -10,6 +18,28 @@ mongoose.connect(db_config.MONGO_URI)
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/register', (req, res) => {
+    const user = new User(req.body)
+
+    // 과거버전 문법
+    // user.save((err, doc) => {
+    //     if(err) return res.json({ success: false, err})
+    //     return res.status(200).json({
+    //         success: true
+    //     })
+    // })
+
+    user.save()
+    .then(saveUSer => {
+        return res.status(200).json({
+            success: true
+        })
+    })
+    .catch( err => {
+        return res.json({ success: false, err})
+    })
 })
 
 app.listen(port, () => {
