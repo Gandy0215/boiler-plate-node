9c533397b21da4aa7330ff6564d9b287af485862
dongwoo <dongwoo@scgs.co.kr>
2024-02-16 11:31:11 +0900

    init 초기화 및 DB 연결 설정
    초기화
    exporess 설치
    mongoose 설치
    mongoDB 연결

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const mongoose = require('mongoose')
+var db_config = require('./config/mongo-config.json')
+mongoose.connect(config.MONGO_URI)
+.then(() => console.log('Mongodb Connected....'))
+.catch(err => console.log(err))
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
