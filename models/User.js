7467bb1fee63bf6670b89d5c1066c0fc9c36a614
dongwoo <dongwoo@scgs.co.kr>
2024-02-16 16:57:59 +0900

    feat bcrypt 설치 및 비밀번호 암호화
    1. bcrypt 설치
        npm install bcrypt --save
    2. 비밀번호 암호화 로직 추가

--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const { default: mongoose } = require("mongoose")
+const bcrypt = require("bcrypt")     // 참고 : https://www.npmjs.com/package/bcrypt
+const saltRounds = 10;
+
 
 const userSchema = mongoose.Schema({
     name: {
@@ -24,6 +27,27 @@ const userSchema = mongoose.Schema({
     },
     tockExp: {
         type: Number
+    }
+})
+
+// save 함수 실행 전에 실행되는 함수
+userSchema.pre('save', function( next ) {
+    var user = this
+
+    if(user.isModified('password')) {
+        // 비밀번호를 암호화 시킴.
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            if(err) return next(err)
+
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if(err) return next(err)
+
+                user.password = hash
+                next()
+            });
+        });
+    } else {
+        next()
     }
 })
 
